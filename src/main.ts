a7f6dd8426744fd7709b5b16a72044c753064f2a
André devkcud Albanese <patommmmm@proton.me>
2024-08-07 18:45:43 -0300

    Add typings

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
-import { SwiblyAPI, SwiblyClient } from './classes/client';
+import { SwiblyAPI, SwiblyClient } from "./classes/client";
+import { SwiblyClientOptions } from "./utils/typings/client";
+import { UserModelFull } from "./utils/typings/user";
+import {
+  UserRegisterBody,
+  UserLoginBody,
+  UserUpdateBody,
+} from "./utils/typings/auth";
+import {
+  ValidAPILanguages,
+  FieldNumericalBoolean,
+} from "./utils/typings/generic";
 
 export { SwiblyAPI, SwiblyClient };
+export type {
+  SwiblyClientOptions,
+  UserModelFull as UserModel,
+  UserRegisterBody as RegisterBody,
+  UserLoginBody as LoginBody,
+  UserUpdateBody as UpdateBody,
+  ValidAPILanguages,
+  FieldNumericalBoolean as NumericalBoolean,
+};
